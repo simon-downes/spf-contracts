2ef188fe7bafdaa95ca0268a4cfd47f4c85d361c
Simon Downes <simon@simondownes.co.uk>
2018-07-22 20:38:00 +0100

    Fix incorrect namespace

--- src/profiler/ProfilerAwareTrait.php
+++ src/profiler/ProfilerAwareTrait.php
@@ -3,7 +3,7 @@
  * This file is part of the spf-contracts package which is distributed under the MIT License.
  * See LICENSE.md or go to https://github.com/simon-downes/spf-contracts for full license details.
  */
-namespace spf\contracts\support\profiler;
+namespace spf\contracts\profiler;
 
 /**
  * A trait to satisfy the ProfilerAware interface.
